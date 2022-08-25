1b328f7b0bde01a83e0a8f26a98e2292cabcf28e
lamine2000 <lemzogueye.lg72@gmail.com>
2022-08-25 14:29:37 +0000

    Initial commit

---
+++ src/main/java/com/laminegallo/full_spring_summary_project/FullSpringSummaryProjectApplication.java
@@ -0,0 +1,13 @@
+package com.laminegallo.full_spring_summary_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullSpringSummaryProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FullSpringSummaryProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/laminegallo/full_spring_summary_project/FullSpringSummaryProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.laminegallo.full_spring_summary_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullSpringSummaryProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
